5b3d7d320ea8fec20baa055e60f5e01d72f38bf7
Rustam Talapov <rustam_talapov@epam.com>
2021-01-11 10:29:08 +0200

    Authentication implementation

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,51 @@
 import React, {Component} from 'react';
+import classes from './Auth.module.css';
+import Input from './../../components/ui/input/Input';
+import Button from './../../components/ui/button/Button';
 
 export default class Auth extends Component {
-  render () {
+  enterHandler = () => {
+
+  };
+
+  signUpHandler = () => {
+
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+  };
+
+  render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Auth</h1>
+          <form
+            className={classes.AuthForm}
+            onSubmit={this.submitHandler}
+          >
+            <Input
+              label="Email"
+            />
+            <Input
+              label="Password"
+              errorMessage="Test"
+            />
+            <Button
+              type="success"
+              onClick={this.enterHandler}
+            >
+              Enter
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.signUpHandler}
+            >
+              Sign Up
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }

10bcd936b75cd0f8a96cc7e389be5b83a55935e3
Rustam Talapov <rustam_talapov@epam.com>
2021-01-12 10:25:38 +0200

    Quiz creation implementation

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
+import classes from './QuizCreator.module.css';
+import Button from '../../components/ui/button/Button';
 
 export default class QuizCreator extends Component {
-  render () {
+  submitHandler = event => {
+    event.preventDefault();
+  };
+
+  addQuestionHandler = () => {
+
+  };
+
+  createQuizHandler = () => {
+
+  };
+
+  render() {
     return (
-      <div>
-        <h1>QuizCreator</h1>
+      <div className={classes.QuizCreator}>
+        <div>
+          <h1>Quiz Creation</h1>
+          <form onSubmit={this.submitHandler}>
+            <input type="text"/>
+            <hr/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <select name="" id=""></select>
+            <Button
+              type="primary"
+              onClick={this.addQuestionHandler}
+            >
+              Add Question
+            </Button>
+            <Button
+              type="success"
+              onClick={this.createQuizHandler}
+            >
+              Create Test
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
